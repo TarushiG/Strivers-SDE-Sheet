aab019c160ebc114610437b3a557c912dcc77bd0
Tarushi <91383199+TarushiG@users.noreply.github.com>
2023-09-25 07:03:31 +1000

    Added notes.

--- Day 1/Set Matrix Zeros.cpp
+++ Day 1/Set Matrix Zeros.cpp
@@ -1,20 +1,20 @@
 #include<bits/stdc++.h>
 vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, 
 int n, int m) {
-	int col[m] = {0};
-	int row[n] = {0};
-	for(int i = 0; i<n; i++){
-		for(int j=0; j<m; j++){
-			if(matrix[i][j] == 0){
-				row[i] = 1;
-				col[j] = 1;
+	int col[m] = {0}; //Defining new column for marking purposes.
+	int row[n] = {0}; //Defining new row for marking purposes.
+	for(int i = 0; i<n; i++){ //Traversing through a row.
+		for(int j=0; j<m; j++){ //Traversing through a column. 
+			if(matrix[i][j] == 0){ //If zero is found...
+				row[i] = 1; //Mark new row with 1.
+				col[j] = 1; //Mark new column with 1.
 			}
 		}
 	}
-	for(int i =0; i<n; i++){
-		for(int j=0; j<m; j++){
-			if(row[i] || col[j]){
-				matrix[i][j] = 0;
+	for(int i =0; i<n; i++){ //Traverse through row again.
+		for(int j=0; j<m; j++){ //Traverse through column again.
+			if(row[i] || col[j]){ //If new row/column marked with 1...
+				matrix[i][j] = 0; //Set all elements of matrix with zero.
 			}
 		}
 	}
